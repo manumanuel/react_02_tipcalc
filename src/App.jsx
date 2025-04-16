538c052b3eff7f53d1a5baab6359520e1c0be141
Manu Manuel <manuelmanu008@gmail.com>
2025-04-16 06:54:33 +0530

    initial commit of tip calculator

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
